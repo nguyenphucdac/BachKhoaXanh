8ede582e44ed31628f44d776bdbeda603a8826c2
NGUYENPHUCDAC\Dac <nguyenphucdac@gmail.com>
2018-03-29 13:57:54 +0700

    commit chức năng tìm kiếm đường đi

---
+++ BachKhoaXanh/src/main/java/com/dsd26/bachkhoaxanh/business/TimDuongDi.java
@@ -0,0 +1,114 @@
+package com.dsd26.bachkhoaxanh.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimDuongDi {
+	public static int MAX_COST = 1000;// gia tri chi phi lon nhat
+	public static int[][] minCost = new int[Map.Size_X][Map.Size_Y];
+	public static boolean[][] freePoint = new boolean[Map.Size_X][Map.Size_Y];
+	public static Point[][] trace = new Point[Map.Size_X][Map.Size_Y];
+	public static List<Point> resultWay = new ArrayList<>();
+	
+	public static List<Point> timDuongDi(Point startPoint, Point targetPoint) {
+		System.out.println("Dang tim kiem xin doi...!!!");
+		
+		Point currPoint = new Point(0, 0);
+		int min;
+		
+		while(true) {
+			min = MAX_COST;
+			for(int i = 0 ; i < Map.Size_X ; i++) {
+				for(int j = 0 ; j < Map.Size_Y; j++) {
+					if( freePoint[i][j] == true && minCost[i][j] < min &&  Map.map[i][j] == 4) {
+						min = minCost[i][j];
+						currPoint.x = i;
+						currPoint.y = j;
+					}
+				}
+			}
+			
+			if(currPoint.x == targetPoint.x && currPoint.y == targetPoint.y) {
+				break;
+			}
+			
+			freePoint[currPoint.x][currPoint.y] = false;
+			
+			if( 	freePoint[currPoint.x + 1][currPoint.y] == true &&
+					isInMap(currPoint.x + 1, currPoint.y) == true && 
+					Map.map[currPoint.x + 1][currPoint.y] == 4 &&
+					minCost[currPoint.x + 1][currPoint.y] >  minCost[currPoint.x][currPoint.y] + 1
+					
+			  ) 
+			{
+				
+				minCost[currPoint.x + 1][currPoint.y] = minCost[currPoint.x][currPoint.y] + 1;
+				trace[currPoint.x + 1][currPoint.y] = new Point(currPoint.x, currPoint.y);
+				
+			}
+			
+			
+			if( 	freePoint[currPoint.x - 1][currPoint.y] == true &&
+					isInMap(currPoint.x - 1, currPoint.y) == true && 
+					Map.map[currPoint.x - 1][currPoint.y] == 4 &&
+					minCost[currPoint.x - 1][currPoint.y] >  minCost[currPoint.x][currPoint.y] + 1
+					
+			  ) 
+			{
+				minCost[currPoint.x - 1][currPoint.y] = minCost[currPoint.x][currPoint.y] + 1;
+				trace[currPoint.x - 1][currPoint.y] = new Point(currPoint.x, currPoint.y);
+				
+			}
+			if( 	freePoint[currPoint.x ][currPoint.y + 1] == true &&
+					isInMap(currPoint.x, currPoint.y + 1) == true && 
+					Map.map[currPoint.x][currPoint.y + 1] == 4 &&
+					minCost[currPoint.x][currPoint.y + 1] >  minCost[currPoint.x][currPoint.y] + 1
+					
+			  ) 
+			{
+				minCost[currPoint.x][currPoint.y + 1] = minCost[currPoint.x][currPoint.y] + 1;
+				trace[currPoint.x][currPoint.y + 1] = new Point(currPoint.x, currPoint.y);
+				
+			}
+			if( 	freePoint[currPoint.x][currPoint.y - 1] == true &&
+					isInMap(currPoint.x, currPoint.y -1) == true && 
+					Map.map[currPoint.x][currPoint.y - 1] == 4 &&
+					minCost[currPoint.x][currPoint.y - 1] >  minCost[currPoint.x][currPoint.y] + 1
+					
+			  ) 
+			{
+				minCost[currPoint.x][currPoint.y - 1] = minCost[currPoint.x][currPoint.y] + 1;
+				trace[currPoint.x][currPoint.y - 1] = new Point(currPoint.x, currPoint.y);
+				
+			}
+			
+		}
+		
+		
+		System.out.println("ket qua tim kiem duong di ngan nhat :");
+		resultWay.add(targetPoint);
+		int i = targetPoint.x, j = targetPoint.y;
+		while(true) {
+			Point point = new Point(trace[i][j].x, trace[i][j].y);
+			resultWay.add(point);
+			
+			i = point.x;
+			j = point.y;
+			
+			if(i == startPoint.x && j == startPoint.y) {	
+				break;
+			}
+		}
+		
+		return resultWay;
+		
+	}
+	
+	public static boolean isInMap(int x, int y) {
+		if(x < 0 || x >= Map.Size_X || y < 0 || y >= Map.Size_Y ) {
+			return false;
+		}
+		return true;
+	}
+	
+}
