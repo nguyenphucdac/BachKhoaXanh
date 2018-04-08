bf577fc72e0b914f0b608682599ebfe867f544a2
NGUYENPHUCDAC\Dac <nguyenphucdac@gmail.com>
2018-04-09 00:44:20 +0700

    thêm class server socket vs chatobject

--- BachKhoaXanh/src/main/java/com/dsd26/bachkhoaxanh/controller/QuanLyController.java
+++ BachKhoaXanh/src/main/java/com/dsd26/bachkhoaxanh/controller/QuanLyController.java
@@ -1,10 +1,12 @@
-package com.dsd26.bachkhoaxanh.controller;
+﻿package com.dsd26.bachkhoaxanh.controller;
 
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.dsd26.bachkhoaxanh.socket.ChatLauncher;
 
 /*
  * author: Nguyễn Phúc Đạc
