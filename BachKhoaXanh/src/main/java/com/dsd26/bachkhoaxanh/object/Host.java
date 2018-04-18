87191d50ec251a3309df3691c8fd2bdf8575c8c0
AMBE1203 <31823215+AMBE1203@users.noreply.github.com>
2018-04-18 16:03:33 +0700

    anh cuong sua host sever nodejs

--- BachKhoaXanh/src/main/java/com/dsd26/bachkhoaxanh/object/Host.java
+++ BachKhoaXanh/src/main/java/com/dsd26/bachkhoaxanh/object/Host.java
@@ -2,6 +2,6 @@ package com.dsd26.bachkhoaxanh.object;
 
 public class Host {
 	private static String host = "http://localhost:9999/";
-	public static String hostNode = "http://10.10.46.107:3000/";;
+	public static String hostNode = "http://10.10.60.235:3000/";;
 	
 }
